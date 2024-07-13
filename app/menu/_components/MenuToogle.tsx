53f6af7bd2ca28d3f6559c5a288131bbb49a3664
thecode-master-sy <thecodemasster.com>
2024-07-13 11:06:48 +0100

    fix: add type to props in MenuToogle path

--- app/menu/_components/MenuToogle.tsx
+++ app/menu/_components/MenuToogle.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 
-const Path = (props) => (
+const Path = (props: any) => (
 	<motion.path
 		fill="transparent"
 		strokeWidth="1"

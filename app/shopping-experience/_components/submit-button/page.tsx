56dc738191e35b56e82673829ea9602a696f86c5
thecode-master-sy <thecodemasster@gmail.com>
2025-06-08 11:59:38 +0100

    feat: shopping experience animation

--- app/shopping-experience/_components/submit-button/page.tsx
+++ app/shopping-experience/_components/submit-button/page.tsx
@@ -2,15 +2,18 @@
 import { ArrowRight } from "lucide-react";
 import { motion } from "motion/react";
 import { MotionValue } from "motion/react";
+import { Dispatch, SetStateAction } from "react";
 
 export default function SubmitButton({
   primaryColor,
   accentColor,
   backgroundColor,
+  setIsAddingNote,
 }: {
   primaryColor: MotionValue<string>;
   accentColor: MotionValue<string>;
   backgroundColor: MotionValue<string>;
+  setIsAddingNote: Dispatch<SetStateAction<boolean>>;
 }) {
   return (
     <div className="w-full">
@@ -18,12 +21,13 @@ export default function SubmitButton({
         style={{ backgroundColor: accentColor }}
         className="flex rounded-full w-full cursor-text max-w-[400px] mx-auto"
       >
-        <motion.p
+        <motion.button
           style={{ color: primaryColor }}
+          onClick={() => setIsAddingNote(true)}
           className="pl-4 py-[14px] flex-1 font-medium cursor-text"
         >
           Add note
-        </motion.p>
+        </motion.button>
         <motion.button
           style={{ backgroundColor: primaryColor, color: backgroundColor }}
           className="flex items-center gap-2 rounded-full px-7 font-bold"
--- app/shopping-experience/page.tsx
+++ app/shopping-experience/page.tsx
@@ -3,19 +3,21 @@ import RightEye from "./_components/right-eye";
 import LeftEye from "./_components/left-eye";
 import { Info, X } from "lucide-react";
 import Mouth from "./_components/mouth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Control from "./_components/control";
 import {
   useMotionValue,
   useAnimation,
   useTransform,
   motion,
+  AnimatePresence,
 } from "motion/react";
 import { CONTROL_CONSTRAIT } from "./_components/constants";
 import ExperienceText from "./_components/experience-text";
 import SubmitButton from "./_components/submit-button/page";
 
 export default function ShoppingExperience() {
+  const [isAddingNote, setIsAddingNote] = useState(false);
   const controlX = useMotionValue(CONTROL_CONSTRAIT);
   const backgroundColor = useTransform(
     controlX,
@@ -33,8 +35,15 @@ export default function ShoppingExperience() {
     ["#798E1A", "#AD750E", "#DA4B23"]
   );
 
+  useEffect(() => {
+    console.log(isAddingNote);
+  }, [isAddingNote]);
+
   return (
-    <motion.div className="min-h-screen p-4" style={{ backgroundColor }}>
+    <motion.div
+      className="min-h-screen flex flex-col p-4"
+      style={{ backgroundColor }}
+    >
       <div className="flex items-center justify-between">
         <motion.span
           style={{ backgroundColor: accentColor }}
@@ -49,38 +58,49 @@ export default function ShoppingExperience() {
           <Info size={20} />
         </motion.span>
       </div>
-      <div className="text-[#153301] text-center mt-4 flex justify-center text-xl">
-        <p>How was your shopping experience?</p>
-      </div>
-      <div className="flex justify-center gap-4 mt-4">
-        <LeftEye controlX={controlX} fill={primaryColor} />
-        <RightEye controlX={controlX} fill={primaryColor} />
-      </div>
+      <motion.div className="flex-1 flex flex-col justify-center">
+        <div className="text-[#153301] text-center flex justify-center text-xl">
+          <p>How was your shopping experience?</p>
+        </div>
+        <div className="flex justify-center gap-4 mt-4">
+          <LeftEye controlX={controlX} fill={primaryColor} />
+          <RightEye controlX={controlX} fill={primaryColor} />
+        </div>
 
-      <div className="flex justify-center">
-        <Mouth controlX={controlX} fill={primaryColor} />
-      </div>
+        <div className="flex justify-center">
+          <Mouth controlX={controlX} fill={primaryColor} />
+        </div>
 
-      <div className="flex justify-center text-center mt-4">
-        <ExperienceText controlX={controlX} accentColor={accentColor} />
-      </div>
+        <AnimatePresence>
+          {!isAddingNote && (
+            <motion.div
+              key="expereince-text"
+              exit={{ x: "-100%", opacity: 0 }}
+              className="flex justify-center overflow-hidden text-center mt-4"
+            >
+              <ExperienceText controlX={controlX} accentColor={accentColor} />
+            </motion.div>
+          )}
+        </AnimatePresence>
 
-      <div className="mt-7 flex justify-center">
-        <Control
-          controlX={controlX}
-          primaryColor={primaryColor}
-          accentColor={accentColor}
-          backgroundColor={backgroundColor}
-        />
-      </div>
+        <div className="mt-7 flex justify-center">
+          <Control
+            controlX={controlX}
+            primaryColor={primaryColor}
+            accentColor={accentColor}
+            backgroundColor={backgroundColor}
+          />
+        </div>
 
-      <div className="mt-20 flex justify-center">
-        <SubmitButton
-          primaryColor={primaryColor}
-          accentColor={accentColor}
-          backgroundColor={backgroundColor}
-        />
-      </div>
+        <div className="mt-20 flex justify-center">
+          <SubmitButton
+            primaryColor={primaryColor}
+            accentColor={accentColor}
+            backgroundColor={backgroundColor}
+            setIsAddingNote={setIsAddingNote}
+          />
+        </div>
+      </motion.div>
     </motion.div>
   );
 }

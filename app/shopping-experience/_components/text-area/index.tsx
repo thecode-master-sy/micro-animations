606237f684931fe1163219a28990fd08c45dbaf7
thecode-master-sy <thecodemasster@gmail.com>
2025-06-08 13:57:51 +0100

    feat: shopping experience animation

--- app/shopping-experience/_components/text-area/index.tsx
+++ app/shopping-experience/_components/text-area/index.tsx
@@ -5,6 +5,7 @@ export default function AddNoteTextArea() {
     <div className="p-4 grid bg-[#c6df53] border-2 rounded-2xl border-shopping-primary">
       <textarea
         placeholder="Add note"
+        autoFocus
         className="text-shopping-primary min-h-[100px] placeholder:text-shopping-primary w-full resize-none bg-transparent focus:outline-0 focus:border-none font-medium"
       />
       <button className="flex rounded-full text-shopping-background px-6 py-4 gap-2 items-center text ml-auto bg-shopping-primary">
--- app/shopping-experience/page.tsx
+++ app/shopping-experience/page.tsx
@@ -143,9 +143,9 @@ export default function ShoppingExperience() {
           {isAddingNote && (
             <motion.div
               key="text-area"
-              initial={{ opacity: 0, y: "10" }}
-              animate={{ opacity: 1, y: "0" }}
-              exit={{ opacity: 0, y: "10" }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
               transition={{
                 ease: "easeInOut",
                 duration: 0.5,

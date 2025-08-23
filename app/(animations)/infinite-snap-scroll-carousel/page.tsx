69aeab9afb4db1b0e9db101c2678ae81d51ef1d1
thecode-master-sy <thecodemasster@gmail.com>
2025-08-23 12:23:47 +0100

    Revert last 3 commits

--- app/(animations)/infinite-snap-scroll-carousel/page.tsx
+++ app/(animations)/infinite-snap-scroll-carousel/page.tsx
@@ -20,8 +20,6 @@ export default function InfiniteSnapScrollCarousel() {
       gestureOrientation: "both",
       wheelMultiplier: 2,
       infinite: true,
-      syncTouch: true,
-      touchMultiplier: 2,
     });
 
     // Listen for the scroll event and log the event data
